aca3748a271b0b1afcba9a3e8877221a277ac2e7
Imre Varga <vargaimi990330@gmail.com>
2021-05-26 14:11:53 +0200

    Models

---
+++ egyuttesek/src/main/java/flowacademy/egyuttesek/model/MusicService.java
@@ -0,0 +1,12 @@
+package flowacademy.egyuttesek.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MusicService {
+    private String id;
+    private String name;
+
+}
